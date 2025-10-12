f91d3ab57f7290c6a2d5bb1f7812a5973295b8da
Ibón Balea <ibon4301@gmail.com>
2025-10-12 02:23:32 +0200

    Login Implementation

---
+++ CorteCheco/Vistas/frmLogin.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CorteCheco
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            pnlHeader = new Guna.UI2.WinForms.Guna2Panel();
+            picLogo = new Guna.UI2.WinForms.Guna2PictureBox();
+            txtUsuario = new Guna.UI2.WinForms.Guna2TextBox();
+            txtPassword = new Guna.UI2.WinForms.Guna2TextBox();
+            btnIngresar = new Guna.UI2.WinForms.Guna2Button();
+            pnlHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            pnlHeader.Controls.Add(picLogo);
+            pnlHeader.CustomizableEdges = customizableEdges3;
+            pnlHeader.Dock = DockStyle.Top;
+            pnlHeader.FillColor = Color.FromArgb(255, 192, 0);
+            pnlHeader.Location = new Point(0, 0);
+            pnlHeader.Name = "pnlHeader";
+            pnlHeader.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            pnlHeader.Size = new Size(380, 180);
+            pnlHeader.TabIndex = 0;
+            // 
+            // picLogo
+            // 
+            picLogo.CustomizableEdges = customizableEdges1;
+            picLogo.Image = Properties.Resources.cortechecologo;
+            picLogo.ImageRotate = 0F;
+            picLogo.Location = new Point(125, 30);
+            picLogo.Name = "picLogo";
+            picLogo.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            picLogo.Size = new Size(120, 120);
+            picLogo.SizeMode = PictureBoxSizeMode.Zoom;
+            picLogo.TabIndex = 0;
+            picLogo.TabStop = false;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.BorderRadius = 15;
+            txtUsuario.CustomizableEdges = customizableEdges5;
+            txtUsuario.DefaultText = "";
+            txtUsuario.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtUsuario.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtUsuario.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtUsuario.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtUsuario.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtUsuario.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsuario.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtUsuario.IconLeft = (Image)resources.GetObject("txtUsuario.IconLeft");
+            txtUsuario.Location = new Point(71, 243);
+            txtUsuario.Margin = new Padding(3, 4, 3, 4);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.PlaceholderText = "Nombre de Usuario";
+            txtUsuario.SelectedText = "";
+            txtUsuario.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            txtUsuario.Size = new Size(229, 48);
+            txtUsuario.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.BorderRadius = 15;
+            txtPassword.CustomizableEdges = customizableEdges7;
+            txtPassword.DefaultText = "";
+            txtPassword.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtPassword.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtPassword.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtPassword.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtPassword.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtPassword.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPassword.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtPassword.IconLeft = (Image)resources.GetObject("txtPassword.IconLeft");
+            txtPassword.Location = new Point(71, 322);
+            txtPassword.Margin = new Padding(3, 4, 3, 4);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderText = "Contraseña";
+            txtPassword.SelectedText = "";
+            txtPassword.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            txtPassword.Size = new Size(229, 48);
+            txtPassword.TabIndex = 2;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.BorderRadius = 15;
+            btnIngresar.CustomizableEdges = customizableEdges9;
+            btnIngresar.DisabledState.BorderColor = Color.DarkGray;
+            btnIngresar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnIngresar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnIngresar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnIngresar.FillColor = Color.FromArgb(255, 192, 0);
+            btnIngresar.Font = new Font("Segoe UI", 9F);
+            btnIngresar.ForeColor = Color.FromArgb(45, 45, 48);
+            btnIngresar.Location = new Point(95, 406);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            btnIngresar.Size = new Size(180, 45);
+            btnIngresar.TabIndex = 3;
+            btnIngresar.Text = "INGRESAR";
+            btnIngresar.Click += this.btnIngresar_Click;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(45, 45, 48);
+            ClientSize = new Size(380, 550);
+            Controls.Add(btnIngresar);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsuario);
+            Controls.Add(pnlHeader);
+            ForeColor = Color.FromArgb(45, 45, 48);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            pnlHeader.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)picLogo).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel pnlHeader;
+        private Guna.UI2.WinForms.Guna2PictureBox picLogo;
+        private Guna.UI2.WinForms.Guna2TextBox txtUsuario;
+        private Guna.UI2.WinForms.Guna2TextBox txtPassword;
+        private Guna.UI2.WinForms.Guna2Button btnIngresar;
+    }
+}
